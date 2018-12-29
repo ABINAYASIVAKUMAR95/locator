bfc9d8f9bf69df54d2198478cd6cc743be6a7322
ABINAYASIVAKUMAR95 <sabinayakumar95@gmail.com>
2018-12-29 23:19:35 +0530

    added methods

---
+++ src/org/locater/Class.java
@@ -0,0 +1,19 @@
+package org.locater;
+
+public class Class {
+	public static void Scooty() {
+		System.out.println("this is new");
+
+	}
+	public static void car() {
+		// TODO Auto-generated method stub
+		System.out.println("this is old");
+
+	}
+	public static void main(String[] args) {
+		car();
+		Scooty();
+	}
+
+
+}
